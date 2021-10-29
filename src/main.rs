d5406f76e3f89ac761d24e5b03df9a2e305d5cf5
sagari <kazankidg@hotmail.co.jp>
2021-10-30 08:55:45 +0900

    FirstCommit：FileAdd

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+mod vars;
+
+fn main() 
+{
+    println!("Hello, world!");
+    vars::run();
+    let i1 : i32 = 3;
+    let i2 : i64 = 3;
+    let i3 = 3;
+
+    println!("Address i1 : {:p}", &i1);
+    println!("Address i2 : {:p}", &i2);
+    println!("Address i3 : {:p}", &i3);
+
+}
